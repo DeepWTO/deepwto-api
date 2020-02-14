034e724506ce2cc6b820de71a519db9aa9d34e86
zachary <syyun@snu.ac.kr>
2020-02-14 16:22:42 +0900

    Set name as deepwto

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pypi-package-template",
+    name="deepwto",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/syyunn/pip",
+    url="https://github.com/syyunn/deepwto-dataset-explore",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
