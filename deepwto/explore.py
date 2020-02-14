c45aa6993f1edb3778e105c3612373b90ada4cdf
zachary <syyun@snu.ac.kr>
2020-02-14 17:11:09 +0900

    Add read_data line-by-line

---
+++ deepwto/explore.py
@@ -0,0 +1,7 @@
+from deepwto import utils
+
+data = utils.read_data("./data/test_data.json")
+print(data)
+
+if __name__ == "__main__":
+    pass
--- deepwto/utils.py
+++ deepwto/utils.py
@@ -1,7 +1,12 @@
 import json
 
 
-def read_json(json_path):
-    with open(json_path) as f:
-        data = json.load(f)
-    return data
+def read_data(json_path):
+    with open(json_path) as fin:
+
+        for each_line in fin:
+            data = json.loads(each_line)
+            test_id = data['testid']
+            features_content_gov = data['gov']
+            features_content_art = data['art']
+            label = data['label']
