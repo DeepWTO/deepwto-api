6935ccc6b6fbab7b63d240826139fc28e8832224
zachary <syyun@snu.ac.kr>
2020-02-15 21:38:19 +0900

    Add commend about how to peak-in

--- deepwto/utils.py
+++ deepwto/utils.py
@@ -3,13 +3,17 @@ import json
 
 def read_data(json_path):
     with open(json_path) as fin:
-        test_ids=[]
         for each_line in fin:
             data = json.loads(each_line)
             test_id = data['testid']
             features_content_gov = data['gov']
             features_content_art = data['art']
             label = data['label']
-            test_ids.append(test_id)
-        for i in test_ids:
-            print(i)
+
+            # Recommend to use debugger to peak into the dataset rather than
+            # using print()
+            # print(test_id)
+            # print(features_content_gov)
+            # print(features_content_art)
+            # print(label)
+
