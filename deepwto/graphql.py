849711edb50ed1cd7ab40447f615362d49658ed0
zachary <syyun@snu.ac.kr>
2020-02-21 19:07:14 +0900

    Add get query for article and label

--- deepwto/graphql.py
+++ deepwto/graphql.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
-from deepwto.constants import available_ds
+from deepwto.constants import available_ds, available_article
 
 
 class AppSyncClient:
     latest_version = '1.0.0'
     available_ds_num = len(available_ds)
     available_ds = available_ds
+
+    available_article_num = len(available_article)
+    available_article = available_article
 
     def __init__(self, api_key, endpoint_url):
         self.api_key = api_key
@@ -28,7 +31,8 @@ class AppSyncClient:
         return response
 
     def get_factual(self, ds: int, version: str = "1.0.0"):
-        assert ds in self.available_ds, "Make sure choose one of ds_num"
+        assert ds in self.available_ds, "Make sure choose ds number from " \
+                                        "available_ds"
         ds = "{}".format(str(ds))
         version = "\"{}\"".format(version)
 
@@ -45,10 +49,50 @@ class AppSyncClient:
         res = self.execute_gql(query).json()
         return res['data']['getFactual']['factual']
 
+    def get_article(self, article: str, version: str = "1.0.0"):
+        assert article in self.available_article, "Make sure choose article " \
+                                                  "from available_article "
+
+        article = "\"{}\"".format(article)
+        version = "\"{}\"".format(version)
+        query = """
+                query GetGATT{{
+                    getGATT(
+                        article: {0}, 
+                        version: {1}) {{
+                               content
+                            }}
+                        }}
+                """.format(article, version)
+
+        res = self.execute_gql(query).json()
+        return res['data']['getGATT']['content']
+
+    def get_label(self, ds:int, article: str, version: str = "1.0.0"):
+        assert ds in self.available_ds, "Make sure choose ds number from " \
+                                        "available_ds"
+        assert article in self.available_article, "Make sure choose article " \
+                                                  "from available_article "
+
+        ds_art = "\"{}\"".format(str(ds)+"_"+article)
+        version = "\"{}\"".format(version)
+        query = """
+                query GetLabel{{
+                    getLabel(
+                        ds_art: {0}, 
+                        version: {1}) {{
+                               cited
+                            }}
+                        }}
+                """.format(ds_art, version)
+
+        res = self.execute_gql(query).json()
+        return res['data']['getLabel']['cited']
+
 
 if __name__ == "__main__":
 
-    api_key = "da2-hgitmt7z45he3mlvrltgmwqlhm"
+    api_key = ""
     endpoint_url = "https://3oedq5prgveqvdax2xtkc2lv34.appsync-api.us-east-1.amazonaws.com/graphql"
 
     client = AppSyncClient(api_key=api_key, endpoint_url=endpoint_url)
@@ -58,6 +102,16 @@ if __name__ == "__main__":
 
     factual = client.get_factual(ds=2)
     print(factual)
+    #
+    print(client.available_article)
+    print(client.available_article_num)
+
+    content = client.get_article(article='Article I')
+    print(content)
+
+    cited = client.get_label(article='Article I', ds=2)
+    print(cited)
+
     # ds = 1
     #
     # query = """
