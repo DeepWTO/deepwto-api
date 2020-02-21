2b7975569761ae8852f475aa110c7a457c06a7cd
zachary <syyun@snu.ac.kr>
2020-02-21 10:27:05 +0900

    Check the current data synced w. cited.yaml

--- deepwto/read_json.py
+++ deepwto/read_json.py
@@ -1,21 +1,37 @@
 import json
+import yaml
 
 
-def read_data(json_path, stop_idx):
+def read_data(json_path, stop_idx, cited):
     read_idx = 0
     ds_numbs = []
     art_numbs = []
+
     with open(json_path) as fin:
         for each_line in fin:
             data = json.loads(each_line)
             test_id = data['testid']
             features_content_gov = data['gov']
             features_content_art = data['art']
-            label = data['label']
-            print(test_id, label)
+            label = int(data['label'][0])
+            # print(test_id, label)
 
             ds_numb = test_id.split('_')[0]
-            art_numb = test_id.split('_')[1]
+            art_numb = test_id.split('_')[1][1:-1]
+            cited_art_numb = cited[art_numb].split(', ')
+
+            # print(ds_numb, art_numb, label)
+            # print(cited_art_numb)
+
+            if label == 0:
+                # print('DS{}'.format(ds_numb))
+                assert 'DS{}'.format(ds_numb) not in cited_art_numb
+            elif label == 1:
+                print(ds_numb, art_numb, label)
+                print(cited_art_numb)
+                print('DS{}'.format(ds_numb))
+                assert 'DS{}'.format(ds_numb) in cited_art_numb
+
             ds_numbs.append(ds_numb)
             art_numbs.append(art_numb)
 
@@ -38,11 +54,20 @@ def read_data(json_path, stop_idx):
 
 
 if __name__ == "__main__":
+    with open("./data/cited.yaml", 'r') as stream:
+        try:
+            cited = yaml.safe_load(stream)
+            keys = list(cited.keys())
+            keys.sort()
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
     test_inst_num = 2287
     train_inst_num = 9153
-    read_data("./data/test_data.json", 0)
+    # read_data("./data/test_data.json", 0, cited)
 
-    # read_data("./data/test_data.json", test_inst_num-1)
-    # read_data("./data/train_data.json", train_inst_num-1)
+    # read_data("./data/test_data.json", test_inst_num-1, cited)
+    read_data("./data/train_data.json", train_inst_num-1, cited)
 
     pass
