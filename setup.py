25e14765c967f960de65af717f7cfcd866ae8547
zachary <syyun@snu.ac.kr>
2020-02-21 13:40:19 +0900

    Add description and require pyyaml

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="deepwto",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="zcryoon",
+    author_email="syyun@snu.ac.kr",
+    description="DeepWTO Database API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/syyunn/deepwto-dataset-explore",
+    url="https://github.com/DeepWTO/deepwto-api",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['pyyaml'],
     python_requires='>=3.6',
 )
